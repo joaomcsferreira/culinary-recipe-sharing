761ccfac65c064e9e6f03b5379409fb09f8ac832
joaomcsferreira <marcosc024@gmail.com>
2023-07-25 16:46:55 -0300

    Initial commit

---
+++ src/main/java/com/joaomarcos/culinaryrecipesharing/CulinaryRecipeSharingApplication.java
@@ -0,0 +1,13 @@
+package com.joaomarcos.culinaryrecipesharing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CulinaryRecipeSharingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CulinaryRecipeSharingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/joaomarcos/culinaryrecipesharing/CulinaryRecipeSharingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaomarcos.culinaryrecipesharing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CulinaryRecipeSharingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
